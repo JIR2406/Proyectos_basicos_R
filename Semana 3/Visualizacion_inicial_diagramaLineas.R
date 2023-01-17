a2e358933896c862e86121caa2bc1e14381f77dc
Jair Garduño <jairgarduno47@gmail.com>
2023-01-17 17:16:38 -0600

    Revert "Semana 3 - 4"
    This reverts commit a78a29b71c574873ecdf4767c52c993529310fdf.

--- Semana 3/Visualizacion_inicial_diagramaLineas.R
+++
@@ -1,9 +0,0 @@
-#install.packages("tidyverse")
-#install.packages("ggplot2")
-#install.packages("palmerpenguins")
-library(tidyverse)
-library(ggplot2)
-library(palmerpenguins)
-data("penguins")
-View(penguins)
-#ggplot(data = penguins)+geom_smooth(mapping = aes(x=flipper_length_mm,y=body_mass_g,color=species,linetype=species)) #Grafico de barras Linetype hace puntuadas las lineas
